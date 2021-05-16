6e6fe2534f681f9d043d875195aedc37a84754f3
Richard Brooks <richardbrooks2000@gmail.com>
2021-05-16 21:01:59 +0000

    pyth rust lb and examples initial check-in

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # pyth-client-rs
-rust API for on-chain pyth account structure
+
+A rust API for desribing on-chain pyth account structures.  A primer on pyth accounts can be found at https://github.com/pyth-network/pyth-client/blob/main/doc/aggregate_price.md
+
+
+Contains a library for use in on-chain program development and an off-chain example program for loading and printing product reference data and aggregate prices from all devnet pyth accounts.
---
+++ src/lib.rs
@@ -0,0 +1,137 @@
+pub const MAGIC          : u32   = 0xa1b2c3d4;
+pub const VERSION_1      : u32   = 1;
+pub const VERSION        : u32   = VERSION_1;
+pub const MAP_TABLE_SIZE : usize = 640;
+pub const PROD_ACCT_SIZE : usize = 512;
+pub const PROD_HDR_SIZE  : usize = 48;
+pub const PROD_ATTR_SIZE : usize = PROD_ACCT_SIZE - PROD_HDR_SIZE;
+
+// each account has its own type
+#[repr(C)]
+pub enum AccountType
+{
+  Unknown,
+  Mapping,
+  Product,
+  Price
+}
+
+// aggregate and contributing prices are associated with a status
+// only Trading status is valid
+#[repr(C)]
+pub enum PriceStatus
+{
+  Unknown,
+  Trading,
+  Halted,
+  Auction
+}
+
+// ongoing coporate action event - still undergoing dev
+#[repr(C)]
+pub enum CorpAction
+{
+  NoCorpAct
+}
+
+// different types of prices associated with a product
+#[repr(C)]
+pub enum PriceType
+{
+  Unknown,
+  Price,
+  TWAP,
+  Volatility
+}
+
+// solana public key
+#[repr(C)]
+pub struct AccKey
+{
+  pub val: [u8;32]
+}
+
+// Mapping account structure
+#[repr(C)]
+pub struct Mapping
+{
+  pub magic      : u32,        // pyth magic number
+  pub ver        : u32,        // program version
+  pub atype      : u32,        // account type
+  pub size       : u32,        // account used size
+  pub num        : u32,        // number of product accounts
+  pub unused     : u32,
+  pub next       : AccKey,     // next mapping account (if any)
+  pub products   : [AccKey;MAP_TABLE_SIZE]
+}
+
+// Product account structure
+#[repr(C)]
+pub struct Product
+{
+  pub magic      : u32,        // pyth magic number
+  pub ver        : u32,        // program version
+  pub atype      : u32,        // account type
+  pub size       : u32,        // price account size
+  pub px_acc     : AccKey,     // first price account in list
+  pub attr       : [u8;PROD_ATTR_SIZE] // key/value pairs of reference attr.
+}
+
+// contributing or aggregate price component
+#[repr(C)]
+pub struct PriceInfo
+{
+  pub price      : i64,        // product price
+  pub conf       : u64,        // confidence interval of product price
+  pub status     : PriceStatus,// status of price (Trading is valid)
+  pub corp_act   : CorpAction, // notification of any corporate action
+  pub pub_slot   : u64
+}
+
+// latest component price and price used in aggregate snapshot
+#[repr(C)]
+pub struct PriceComp
+{
+  publisher  : AccKey,         // key of contributing quoter
+  agg        : PriceInfo,      // contributing price to last aggregate
+  latest     : PriceInfo       // latest contributing price (not in agg.)
+}
+
+// Price account structure
+#[repr(C)]
+pub struct Price
+{
+  pub magic      : u32,        // pyth magic number
+  pub ver        : u32,        // program version
+  pub atype      : u32,        // account type
+  pub size       : u32,        // price account size
+  pub ptype      : PriceType,  // price or calculation type
+  pub expo       : i32,        // price exponent
+  pub num        : u32,        // number of component prices
+  pub unused     : u32,
+  pub curr_slot  : u64,        // currently accumulating price slot
+  pub valid_slot : u64,        // valid slot-time of agg. price
+  pub prod       : AccKey,     // product account key
+  pub next       : AccKey,     // next Price account in linked list
+  pub agg_pub    : AccKey,     // quoter who computed last aggregate price
+  pub agg        : PriceInfo,  // aggregate price info
+  pub comp       : [PriceComp;16] // price components one per quoter
+}
+
+struct AccKeyU64
+{
+  pub val: [u64;4]
+}
+
+pub fn cast<T>( d: &[u8] ) -> &T {
+  let (_, pxa, _) = unsafe { d.align_to::<T>() };
+  &pxa[0]
+}
+
+impl AccKey
+{
+  pub fn is_valid( &self ) -> bool  {
+    let k8 = cast::<AccKeyU64>( &self.val );
+    return k8.val[0]!=0 || k8.val[1]!=0 || k8.val[2]!=0 || k8.val[3]!=0;
+  }
+}
