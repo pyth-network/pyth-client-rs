1c1c2833514750490422bf8d1b1a38261da26b8e
Jayant Krishnamurthy <jayantkrishnamurthy@gmail.com>
2022-01-05 09:12:08 -0800

    Update client library (#13)
    * Helper method for pricing a base currency in a quote currency (#8)
    
    * Add method for getting twap
    
    * initial implementation, seems to work
    
    * minor
    
    * minor
    
    * refactor
    
    * found bad case
    
    * use u128
    
    * working on it
    
    * clarify
    
    * cleanup
    
    * more cleanup
    
    * pretty sure i need this
    
    * better
    
    * bad merge
    
    * no println
    
    * adding solana tx stuff
    
    * change approach a bit
    
    * this seems to work
    
    * comment
    
    * cleanup
    
    * refactor
    
    * refactor
    
    * initial implementation of mul
    
    * exponent
    
    * tests for normalize
    
    * tests for normalize
    
    * negative numbers in div
    
    * handle negative numbers
    
    * comments
    
    * stuff
    
    * cleanup
    
    * unused
    
    * minor
    
    Co-authored-by: Jayant Krishnamurthy <jkrishnamurthy@jumptrading.com>
    
    * Instruction counts and optimizations (#9)
    
    * instruction counts
    
    * reduce normalize opcount
    
    * instruction counts
    
    * tests
    
    Co-authored-by: Jayant Krishnamurthy <jkrishnamurthy@jumptrading.com>
    
    * Docs and utilities (#12)
    
    * uh oh
    
    * docs
    
    * fix error docs
    
    Co-authored-by: Jayant Krishnamurthy <jkrishnamurthy@jumptrading.com>
    
    * bump version number
    
    * bump version number
    
    * ignore more files
    
    * docs
    
    * remove mod
    
    * checked ops
    
    Co-authored-by: Jayant Krishnamurthy <jkrishnamurthy@jumptrading.com>

---
+++ src/instruction.rs
@@ -0,0 +1,96 @@
+//! Program instructions for end-to-end testing and instruction counts
+
+use {
+  crate::id,
+  borsh::{BorshDeserialize, BorshSerialize},
+  solana_program::instruction::Instruction,
+  crate::PriceConf,
+};
+
+/// Instructions supported by the pyth-client program, used for testing and
+/// instruction counts
+#[derive(Clone, Debug, BorshSerialize, BorshDeserialize, PartialEq)]
+pub enum PythClientInstruction {
+  Divide {
+    numerator: PriceConf,
+    denominator: PriceConf,
+  },
+  Multiply {
+    x: PriceConf,
+    y: PriceConf,
+  },
+  Add {
+    x: PriceConf,
+    y: PriceConf,
+  },
+  ScaleToExponent {
+    x: PriceConf,
+    expo: i32,
+  },
+  Normalize {
+    x: PriceConf,
+  },
+  /// Don't do anything for comparison
+  ///
+  /// No accounts required for this instruction
+  Noop,
+}
+
+pub fn divide(numerator: PriceConf, denominator: PriceConf) -> Instruction {
+  Instruction {
+    program_id: id(),
+    accounts: vec![],
+    data: PythClientInstruction::Divide { numerator, denominator }
+      .try_to_vec()
+      .unwrap(),
+  }
+}
+
+pub fn multiply(x: PriceConf, y: PriceConf) -> Instruction {
+  Instruction {
+    program_id: id(),
+    accounts: vec![],
+    data: PythClientInstruction::Multiply { x, y }
+      .try_to_vec()
+      .unwrap(),
+  }
+}
+
+pub fn add(x: PriceConf, y: PriceConf) -> Instruction {
+  Instruction {
+    program_id: id(),
+    accounts: vec![],
+    data: PythClientInstruction::Add { x, y }
+      .try_to_vec()
+      .unwrap(),
+  }
+}
+
+pub fn scale_to_exponent(x: PriceConf, expo: i32) -> Instruction {
+  Instruction {
+    program_id: id(),
+    accounts: vec![],
+    data: PythClientInstruction::ScaleToExponent { x, expo }
+      .try_to_vec()
+      .unwrap(),
+  }
+}
+
+pub fn normalize(x: PriceConf) -> Instruction {
+  Instruction {
+    program_id: id(),
+    accounts: vec![],
+    data: PythClientInstruction::Normalize { x }
+      .try_to_vec()
+      .unwrap(),
+  }
+}
+
+/// Noop instruction for comparison purposes
+pub fn noop() -> Instruction {
+  Instruction {
+    program_id: id(),
+    accounts: vec![],
+    data: PythClientInstruction::Noop.try_to_vec().unwrap(),
+  }
+}
---
+++ src/processor.rs
@@ -0,0 +1,45 @@
+//! Program instruction processor for end-to-end testing and instruction counts
+
+use borsh::BorshDeserialize;
+use solana_program::{
+  account_info::AccountInfo,
+  entrypoint::ProgramResult,
+  pubkey::Pubkey,
+};
+
+use crate::{
+  instruction::PythClientInstruction,
+};
+
+pub fn process_instruction(
+  _program_id: &Pubkey,
+  _accounts: &[AccountInfo],
+  input: &[u8],
+) -> ProgramResult {
+  let instruction = PythClientInstruction::try_from_slice(input).unwrap();
+  match instruction {
+    PythClientInstruction::Divide { numerator, denominator } => {
+      numerator.div(&denominator);
+      Ok(())
+    }
+    PythClientInstruction::Multiply { x, y } => {
+      x.mul(&y);
+      Ok(())
+    }
+    PythClientInstruction::Add { x, y } => {
+      x.add(&y);
+      Ok(())
+    }
+    PythClientInstruction::Normalize { x } => {
+      x.normalize();
+      Ok(())
+    }
+    PythClientInstruction::ScaleToExponent { x, expo } => {
+      x.scale_to_exponent(expo);
+      Ok(())
+    }
+    PythClientInstruction::Noop => {
+      Ok(())
+    }
+  }
+}
